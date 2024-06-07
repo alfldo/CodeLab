03a3a10c939bd8c639d8d934afdd60e525b41cb1
alfldo <alfldo1012@naver.com>
2024-06-07 21:01:04 +0900

    modified:   baekjoon/gold/2636.cpp

--- baekjoon/gold/2636.cpp
+++ baekjoon/gold/2636.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main()
 {
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int n,m;
+    cin >> n >> m;
+    vector< vector<int> > v(n,vector<int>(m,0));
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            cin>> v[i][j];
+        }
+    }
 }

